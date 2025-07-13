1c7d3f5800387ca94a9cea3a49d72bed839b90af
Long Nguyen <faker2407@gmail.com>
2025-07-13 15:33:50 +0700

    add error handling demo in /test - create a divide function + if denominator = 0, return error + else return result numerator / denumerator

---
+++ test/error-handling.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Item struct {
+	ID int
+	Name string
+}
+
+func divide(numerator int, denominator int) (int, error) {
+	if denominator == 0 {
+		return 0, errors.New("can not devide by zero")
+	}
+
+	result := numerator / denominator
+	return  result, nil
+}
+
+func main() {
+	result, err := divide(10,2)
+	if err != nil {
+		fmt.Println("error:", err)
+	} else {
+		fmt.Println("Result is:", result)
+	}
+	fmt.Println("-------------------------")
+	result1, err1 := divide(10,0)
+	if err1 != nil {
+		fmt.Println("error:", err1)
+	} else {
+		fmt.Println("Result is:", result1)
+	}
+}
